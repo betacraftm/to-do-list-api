508b7178c195b20f67e5c6416af983af166c1172
Betacraft <hoangkimc9@gmail.com>
2024-04-14 13:14:37 +0700

    Add localhost url for debug

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['https://to-do-list-qu8a.onrender.com']
+const allowedOrigins = [
+  'https://to-do-list-qu8a.onrender.com',
+  'http://localhost:5173',
+]
 
 module.exports = allowedOrigins
